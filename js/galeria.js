bad0538dde07603dd0906a62f067275161a15ab6
Chris <christianrojas.adrian@gmail.com>
2026-09-11 15:39:52 -0300

    Estructura de la segunda página
    Mi idea para la galería de fotos es poner un boton que cambie el color de fondo de las imagenes

---
+++ js/galeria.js
@@ -0,0 +1,9 @@
+// array con las obras
+let obras = [
+    { nombre: "Blow Up", anio: 2007, imagen: "./img/obra01.jpg" },
+    { nombre: "Navier-Stokes", anio: 2009, imagen: "./img/obra02.jpg" },
+    { nombre: "Voice Array", anio: 2011, imagen: "./img/obra03.jpg" },
+    { nombre: "Kristallstimmen", anio: 2024, imagen: "./img/obra04.jpg" },
+    { nombre: "Surface Tension", anio: 1992, imagen: "./img/obra05.jpg" }
+];
+
